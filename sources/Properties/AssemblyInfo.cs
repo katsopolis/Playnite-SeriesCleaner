ab26aea813c134785d64574d1a5417a5d39f9694
katsopolis <genessedef@gmail.com>
2025-03-17 03:00:17 +0300

    Initial commit

--- sources/Properties/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Series Cleaner")]
-[assembly: AssemblyDescription("Removes Series field entries for games that are the only title in their series.")]
-[assembly: AssemblyCompany("Your Name")]
-[assembly: AssemblyProduct("Series Cleaner")]
-[assembly: AssemblyCopyright("Copyright © Your Name 2025")]
